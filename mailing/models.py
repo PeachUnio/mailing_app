eb23c23c51924d9dbdf5e8f524d66dd7a25fa662
Арина Новгородова <arrrishanov@gmail.com>
2026-01-23 21:57:15 +0300

    add MailingRecipient model

--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class MailingRecipient(models.Model):
+    email = models.EmailField(unique=True, max_length=100, verbose_name="Почта получателя", help_text="Введите почту получателя")
+    name = models.CharField(max_length=100, verbose_name="Имя получателя",help_text="Введите ФИО получателя")
+    comment = models.TextField(null=True, blank=True, verbose_name="Комментарий")
