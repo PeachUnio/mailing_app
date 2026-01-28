3a82af9cfed98b31b61ada0a458deb5331ab7eb3
Арина Новгородова <arrrishanov@gmail.com>
2026-01-28 22:09:37 +0300

    register models from mailing app

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import MailingRecipient, Message, Mailing
+
+
+@admin.register(MailingRecipient)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "name")
+    list_filter = ("name",)
+    search_fields = ("name", "email")
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ("id", "letter_theme")
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ("id", "status")
