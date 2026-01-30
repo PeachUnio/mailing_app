8533b1e007e11a5e5a557113b59e569378dd6d00
Арина Новгородова <arrrishanov@gmail.com>
2026-01-30 20:09:07 +0300

    add urls

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path, include
+from django.urls import path
 from users.apps import UsersConfig
+from users.views import UserCreateView, email_verification, PasswordResetRequestView, PasswordResetConfirmView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
+    path("password-reset/", PasswordResetRequestView.as_view(), name="password-reset"),
+    path("password-reset-confirm/<str:token>/", PasswordResetConfirmView.as_view(), name="password-reset-confirm"),
 ]
