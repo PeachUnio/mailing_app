2fa1cb76ec258e482ec5c3f43173350f3eff53f8
Арина Новгородова <arrrishanov@gmail.com>
2026-01-31 19:00:21 +0300

    fix users

--- users/models.py
+++ users/models.py
@@ -43,6 +43,8 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    objects = UserManager()
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
