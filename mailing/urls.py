ee32406903ca35c4b58c86c69ef087af7752843f
Арина Новгородова <arrrishanov@gmail.com>
2026-02-08 14:36:03 +0300

    add cache

--- mailing/urls.py
+++ mailing/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
 
     # Рассылки
-    path('mailings/', views.MailingListView.as_view(), name='mailing_list'),
+    path('mailings/',  cache_page(60 * 15)(views.MailingListView.as_view()), name='mailing_list'),
     path('mailings/create/', views.MailingCreateView.as_view(), name='mailing_create'),
     path('mailings/<int:pk>/', views.MailingDetailView.as_view(), name='mailing_detail'),
     path('mailings/<int:pk>/update/', views.MailingUpdateView.as_view(), name='mailing_update'),
@@ -20,13 +20,13 @@ urlpatterns = [
     path('mailings/<int:pk>/send/', views.send_mailing_now, name='send_mailing'),
 
     # Сообщения
-    path('messages/', views.MessageListView.as_view(), name='message_list'),
+    path('messages/',  cache_page(60 * 15)(views.MessageListView.as_view()), name='message_list'),
     path('messages/create/', views.MessageCreateView.as_view(), name='message_create'),
     path('messages/<int:pk>/update/', views.MessageUpdateView.as_view(), name='message_update'),
     path('messages/<int:pk>/delete/', views.MessageDeleteView.as_view(), name='message_delete'),
 
     # Получатели
-    path('recipients/', views.RecipientListView.as_view(), name='recipient_list'),
+    path('recipients/', cache_page(60 * 15)(views.RecipientListView.as_view()), name='recipient_list'),
     path('recipients/create/', views.RecipientCreateView.as_view(), name='recipient_create'),
     path('recipients/<int:pk>/update/', views.RecipientUpdateView.as_view(), name='recipient_update'),
     path('recipients/<int:pk>/delete/', views.RecipientDeleteView.as_view(), name='recipient_delete'),
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from users.apps import UsersConfig
 from users.views import UserCreateView, email_verification, PasswordResetRequestView, PasswordResetConfirmView, UserListView, ToggleUserActiveView
 
@@ -12,6 +13,6 @@ urlpatterns = [
     path("email-confirm/<str:token>/", email_verification, name="email-confirm"),
     path("password-reset/", PasswordResetRequestView.as_view(), name="password-reset"),
     path("password-reset-confirm/<str:token>/", PasswordResetConfirmView.as_view(), name="password-reset-confirm"),
-    path("list/", UserListView.as_view(), name="user_list"),
+    path("list/", cache_page(60 * 15)(UserListView.as_view()), name="user_list"),
     path("toggle-active/<int:pk>/", ToggleUserActiveView.as_view(), name="toggle_user_active"),
 ]
